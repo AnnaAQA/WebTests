596fa2b248a614c511ce2e3125ccebc90692911c
AnnaAQA <kalinina_anna_spb@mail.ru>
2025-02-23 15:26:13 +0300

    repeat theory code

---
+++ tests/RecoveryTests.py
@@ -0,0 +1,25 @@
+from core.BaseTest import browser
+from pages.BasePage import BasePage
+from pages.LoginPage import LoginPageHelpers
+from pages.RecoveryPage import RecoveryPageHelpers
+import  allure
+
+BASE_URL = 'https://ok.ru/'
+EMAIL_TEXT = 'email'
+PASSWORD_TEXT = 'psw'
+
+
+
+@allure.suite('Проверка восстановления пользователя')
+@allure.title('Проверка перехода к странице восстановления после нескольких неудачных попыток авторизации')
+def test_go_to_recovery_after_many_auth_fails(browser):
+    BasePage(browser).get_url(BASE_URL)
+    LoginPage = LoginPageHelpers(browser)
+    LoginPage.click_field_email_mobile(EMAIL_TEXT)
+
+    for i in range(3):
+        LoginPage.click_field_password(PASSWORD_TEXT)
+        LoginPage.click_login()
+
+    LoginPage.click_recovery()
+    RecoveryPageHelpers(browser)
