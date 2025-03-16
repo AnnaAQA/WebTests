03013b8af27965911930918dfa263b68df61b9f4
AnnaAQA <kalinina_anna_spb@mail.ru>
2025-03-16 16:44:09 +0300

    add server address

--- core/BaseTest.py
+++ core/BaseTest.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 def browser():
     options = webdriver.ChromeOptions()
     options.add_argument("--lang=ru")
-    driver = webdriver.Remote(command_executor="http://5.181.109.28:8080", options=options)
+    driver = webdriver.Remote(command_executor="http://5.181.109.28:4444", options=options)
     yield  driver
     if driver:
         driver.quit()
